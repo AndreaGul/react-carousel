86ac707e347c7d35eb3a3e2d210ece7bbee1d307
AndreaGul <andreagulino99@gmail.com>
2024-06-14 15:46:48 +0200

    fix:rendering immagini

--- posts.js
+++ posts.js
@@ -2,27 +2,27 @@ const posts = [
     {
         id: 0,
         title: "Post 1",
-        image: "https://picsum.photos/id/10/200"
+        image: "https://picsum.photos/id/10/700/500"
     },
     {
         id: 1,
         title: "Post 2",
-        image: "https://picsum.photos/id/11/200"
+        image: "https://picsum.photos/id/11/700/500"
     },
     {
         id: 2,
         title: "Post 3",
-        image: "https://picsum.photos/id/12/200"
+        image: "https://picsum.photos/id/12/700/500"
     },
     {
         id: 3,
         title: "Post 4",
-        image: "https://picsum.photos/id/13/200"
+        image: "https://picsum.photos/id/13/700/500"
     },
     {
         id: 4,
         title: "Post 5",
-        image: "https://picsum.photos/id/14/200"
+        image: "https://picsum.photos/id/14/700/500"
     }
 ]
 
--- src/components/Carosel.jsx
+++ src/components/Carosel.jsx
@@ -2,11 +2,11 @@ import caroselCss from "./Carosel.module.scss"
 
 export default function ({title,image,isVisible}) {
     return(
-        <>{isVisible &&  <div className={caroselCss.container}>
+        <>
+        <div className={caroselCss.container} style={{display: isVisible ? 'block' : 'none'}}>
             <img src={image} alt="Nessun immagine trovata" />
             <h2>{title}</h2>
-        </div>}
-       
+        </div>
        
         </>
     )
